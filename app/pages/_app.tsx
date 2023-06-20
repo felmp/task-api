6eee1cd5f51fb91fb9036bd641aec34f6acc97bf
Francisco Felipe <felipezf1001@gmail.com>
2023-06-20 00:52:24 -0300

    remove: all app project (next.js)

--- app/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import type { AppProps } from 'next/app'
-import Layout from '@/components/Header'
-import '@/styles/globals.css'
-
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-      <Component {...pageProps} />
-  )
-}
